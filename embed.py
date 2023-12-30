425288f0abf1c66e9dae16a65b3e7dc7fc897c4d
Josh Cowan <35657686+joshuajz@users.noreply.github.com>
2023-12-30 01:54:06 -0500

    Inital project

---
+++ embed.py
@@ -0,0 +1,90 @@
+import discord
+import datetime
+import enum
+
+
+class embedColours(enum.Enum):
+    """Colours for embeds"""
+
+    red = 0xB30012
+    orange = 0xFF7B00
+    light_green = 0x1DBF38
+    dark_blue = 0x1D58BF
+    magenta = 0xA832A4
+    cyan = 0x28C0D4
+
+
+def create_embed_template(title: str, description: str, template: str) -> discord.Embed:
+    """Creates an embed from a template
+
+    Args:
+        title (str): Title of the embed
+        description (str): Description of the embed
+        template (str): Which template to use
+
+    Returns:
+        discord.Embed: The embed with the given information
+    """
+
+    if template.lower() == "error":
+        message = discord.Embed(
+            title=title,
+            description=description,
+            colour=embedColours["red"].value,
+            timestamp=datetime.datetime.utcnow(),
+        )
+        message.set_footer(
+            text="Contact JZ.exe with bugs.",
+            icon_url="https://art.pixilart.com/a194f7430c1fc04.png",
+        )
+    return message
+
+
+def create_embed(
+    title: str, description: str, colour: str, footer="contact", thumbnail=None
+) -> discord.Embed:
+    """Creates an embed object
+
+    Args:
+        title (str): Title of the embed
+        description (str): Description of the embed
+        colour (str): The colour (reference enmbedColours)
+        footer (str, optional): An optional footer. Defaults to "contact".
+        thumbnail (str, optional): An optional thumbnail image (link). Defaults to None.
+
+    Returns:
+        discord.Embed: Embed with the given information
+    """
+
+    message = discord.Embed(
+        title=title,
+        description=description,
+        colour=embedColours[colour].value,
+        timestamp=datetime.datetime.utcnow(),
+    )
+
+    if thumbnail is not None:
+        message.set_thumbnail(url=thumbnail)
+
+    if footer == "contact":
+        message.set_footer(
+            text="Created by jz.exe.",
+            icon_url="https://art.pixilart.com/a194f7430c1fc04.png",
+        )
+    else:
+        message.set_footer(text=footer)
+
+    return message
+
+
+def add_field(embed: discord.embeds.Embed, title: str, value: str, inline: bool):
+    """Adds a field to a message
+
+    Args:
+        embed (discord.embeds.Embed): The embed to add to
+        title (str): Title of the field
+        value (str): Description of the field
+        inline (bool): If it's inline
+    """
+
+    embed.add_field(name=title, value=value, inline=inline)
---
+++ main.py
@@ -0,0 +1,225 @@
+import discord
+import os
+from dotenv import load_dotenv
+import json
+import datetime
+from discord.ext import tasks, commands
+import requests
+from embed import create_embed, add_field
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+def call_api():
+    return requests.get('https://www.nytimes.com/svc/wordle/v2/2023-12-26.json').json()
+
+def read_database():
+    with open('database.json', 'r') as database:
+        data = None
+        if (os.path.isfile(os.getcwd() + '\\database.json')) and (os.path.getsize(os.getcwd() + '\\database.json') > 0):
+            data = json.load(database)
+        else:
+            data = {}
+        
+        if not(data):
+            data = {}
+    return data
+
+def read_stats():
+    with open('stats.json', 'r') as database:
+        data = None
+        if (os.path.isfile(os.getcwd() + '\\stats.json')) and (os.path.getsize(os.getcwd() + '\\stats.json') > 0):
+            data = json.load(database)
+        else:
+            data = {}
+        
+        if not(data):
+            data = {}
+    return data
+
+
+class MyCog(commands.Cog):
+    def __init__(self):
+        self.wordle_answer = {
+            'id': None,
+            'solution': None,
+            'print_date': None,
+            'days_since_launch': None,
+            'editor': None,
+        }
+        self.newest_day = None
+        self.api_call.start()
+        self.checker.start()
+
+    def cog_unload(self):
+        self.checker.cancel()
+        self.api_call.cancel()
+    
+    def today_wordle(self):
+        data = read_database()
+
+        today = {}
+
+        for player in data.keys():
+            if player in data and str(self.wordle_answer['days_since_launch']) in (data[player]).keys():
+                wordle = data[player][str(self.wordle_answer['days_since_launch'])]
+                ratio = int(wordle['ratio'].split('/')[0])
+                if ratio in today:
+                    today[ratio].append((player, wordle))
+                else:
+                    today[ratio] = [(player, wordle)]
+        
+        return today
+    
+    def best_answer(self, today: dict):
+        minimum = min(today.keys())
+        return today[minimum]
+
+        
+    def worst_answer(self, today: dict):
+        maximum = max(today.keys())
+        return today[maximum]
+
+    def weekly_average(self):
+        last_seven = [i for i in range(int(self.wordle_answer['days_since_launch']), int(self.wordle_answer['days_since_launch']) - 7, -1)]
+        data = read_database()
+        
+        averages = {}
+        for user in data.keys():
+            for day in last_seven:
+                if str(day) in data[user].keys():
+                    day = str(day)
+                    if user in averages:
+                        averages[user][0] += int(data[user][day]['ratio'].split('/')[0])
+                        averages[user][1] += 1
+                    else:
+                        averages[user] = [int(data[user][day]['ratio'].split('/')[0]), 1]
+        result = []
+        for user in averages.keys():
+            result.append((user, averages[user][0] / averages[user][1]))
+        
+        result.sort(key=lambda a: a[1])
+
+        stats_data = read_stats()
+
+        if 'weekly' in stats_data:
+            stats_data['weekly'][str(self.wordle_answer['days_since_launch'])] = result
+        else:
+            stats_data['weekly'] = {str(self.wordle_answer['days_since_launch']): result}
+        with open('stats.json', 'w') as stats_database:
+            json.dump(stats_data, stats_database, indent=4)
+        
+        return result
+
+    def daily_average(self):
+        # Determine the gorup's daily average
+        average = [0, 0]
+
+        data = read_database()
+        for user in data.keys():
+            if str(self.wordle_answer['days_since_launch']) in data[user].keys():
+                average[0] += int(data[user][str(self.wordle_answer['days_since_launch'])]['ratio'].split('/')[0])
+                average[1] += 1
+        
+        return average
+
+    @tasks.loop(seconds=45.0)
+    async def checker(self):
+        hour, minute = datetime.datetime.now().strftime("%H %M").split(" ")
+        if hour == 11 and (minute == 58 or minute == 59) and self.newest_day != self.wordle_answer['days_since_launch']:
+        # if hour == "01" and (minute == "32" or minute == "33") and self.newest_day != self.wordle_answer['days_since_launch']:
+            self.newest_day = self.wordle_answer['days_since_launch']
+        else:
+            return
+        
+        embed = create_embed('Wordle Recap', f'**#{self.wordle_answer["days_since_launch"]}** *{self.wordle_answer["solution"]}* - {self.wordle_answer["print_date"]}', 'orange')
+        
+        guild = await client.fetch_guild(int(os.getenv('server')))
+        channel = await guild.fetch_channel(int(os.getenv('channel_id')))
+        
+        today = self.today_wordle()
+        best_answer = self.best_answer(today)
+        worst_answer = self.worst_answer(today)
+
+        if len(best_answer) == 1:
+            best_answer = best_answer[0]
+            result = f'<@{best_answer[0]}> with **{best_answer[1]["ratio"]}**:'
+            for s in best_answer[1]['board']:
+                print('s', s)
+                result += "\n" + s
+            add_field(embed, f'Winner!', result, True)
+
+        if len(worst_answer) == 1:
+            worst_answer = worst_answer[0]
+            result = f'<@{worst_answer[0]}> with **{worst_answer[1]["ratio"]}**:'
+            for s in worst_answer[1]['board']:
+                result += "\n" + s
+            add_field(embed, f'Loser (just kidding)!', result, True)
+
+        # Weekly Average
+        averages = self.weekly_average()
+        if len(averages) != 0:
+            averages_string = f'<@{averages[0][0]}>: {averages[0][1]}/6'
+            for avg in range(1, len(averages), 1):
+                print('avg', avg)
+                averages_string += '\n'
+                averages_string += f'<@{averages[avg][0]}>: {averages[avg][1]}/6'
+        add_field(embed, f'Weekly Averages', averages_string, False)
+
+        # Daily Average
+        daily_average = self.daily_average()
+        if len(daily_average) == 2:
+            add_field(embed, f"Group's Daily Average", f"{daily_average[0]/daily_average[1]}/6", True)
+
+        await channel.send(embed=embed)
+
+    @tasks.loop(hours=1)
+    async def api_call(self):
+        if int(datetime.datetime.now().strftime("%H")) == 11:
+            return
+        self.wordle_answer = call_api()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    cog = MyCog()
+
+@client.event
+async def on_message(message):
+    print('message.aut')
+    if message.author == client.user:
+        return
+
+    if not(message.content.startswith("Wordle")):
+        return
+    
+    msg = message.content.split('\n')
+    date, ratio = msg[0].split(' ')[1::]
+
+    user_id = message.author.id
+    wordle_board = msg[2::]
+    
+    entry = {'worldle_date': date, 'ratio': ratio, 'date': str(datetime.datetime.now()), 'board': wordle_board}
+
+    data = read_database()    
+
+    with open('database.json', 'w') as database:
+        if str(user_id) in data:
+            data[str(user_id)][date] = entry
+        else:
+            data[str(user_id)] = {date: entry}
+
+        json.dump(data, database, indent=4)
+
+
+    
+
+
+
+
+client.run(os.getenv('token'))
